6164651649774568b97be4a329468157fdcc48bd
Christian <krisztian.radvanyi@gmail.com>
2025-05-16 17:07:40 +0200

    Create project, prepare style sheets

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { Inter, Noto_Sans, Source_Code_Pro } from "next/font/google";
+import "./globals.css";
+
+const interSansSerif = Inter({
+  variable: "--font-inter-sans-serif",
+  subsets: ["latin"],
+});
+
+const notoSans = Noto_Sans({
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+});
+
+const sourceCodeProMono = Source_Code_Pro({
+  variable: "--font-source-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Frontend Mentor | Note-taking web app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${interSansSerif.variable} ${notoSans.variable}  ${sourceCodeProMono.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Note Taking Web App</h1>;
+}
