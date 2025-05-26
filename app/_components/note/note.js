dd56ef3d64097a3d3d04b2049c161048c23e547c
Christian <krisztian.radvanyi@gmail.com>
2025-05-26 19:10:34 +0200

    Bigfix: Fix Note layout to meet the design requirements when there is no note to display.

--- app/_components/note/note.js
+++ app/_components/note/note.js
@@ -12,58 +12,58 @@ export default function Note() {
   const { note } = use(AllNotesCtx);
 
   return (
-    <div className={styles.note}>
-      <div className={styles.panel}>
-        <div className={styles.container}>
-          <header className={styles.header}>
-            <NoteHeader />
-          </header>
-          {note?.error ? (
-            <div className={styles.alternative}>
-              <p className="text-preset-5 text-color-neutral-800">
-                {note.error}
-              </p>
-            </div>
-          ) : note && !note._id ? (
-            <div className={styles.alternative}></div>
-          ) : note ? (
-            <section className={styles.details}>
-              <TextareaAutosize
-                minRows={1}
-                maxRows={3}
-                value={note.title}
-                className={`text-preset-1 text-color-neutral-950 ${styles.title}`}
-                onChange={() => {}}
-                aria-label="Title"
-              />
-              <div className={styles.property}>
-                <div className={styles.propertyName}>
-                  <IconClock className={styles.propertyIcon} />
-                  <p className="text-preset-6">Last edited</p>
-                </div>
-                <p className="text-preset-6">29 Oct 2024</p>
-              </div>
-              <hr />
-              <textarea
-                value={note.content}
-                className={`text-preset-5 text-color-neutral-800 ${styles.content}`}
-                onChange={() => {}}
-                aria-label="Content"
-              />
-            </section>
-          ) : (
-            <div className={styles.alternative}>
-              <p className="text-preset-5 text-color-neutral-800">Loading...</p>
-            </div>
-          )}
-          <footer className={styles.footer}>
-            <NoteFooter />
-          </footer>
+    <>
+      {note?.error ? (
+        <div className={styles.alternative}>
+          <p className="text-preset-5 text-color-neutral-800">{note.error}</p>
         </div>
-      </div>
-      <aside className={styles.sidebar}>
-        <NoteSiderbar />
-      </aside>
-    </div>
+      ) : note && !note._id ? (
+        <div className={styles.alternative}></div>
+      ) : note ? (
+        <div className={styles.note}>
+          <div className={styles.panel}>
+            <div className={styles.container}>
+              <header className={styles.header}>
+                <NoteHeader />
+              </header>
+              <section className={styles.details}>
+                <TextareaAutosize
+                  minRows={1}
+                  maxRows={3}
+                  value={note.title}
+                  className={`text-preset-1 text-color-neutral-950 ${styles.title}`}
+                  onChange={() => {}}
+                  aria-label="Title"
+                />
+                <div className={styles.property}>
+                  <div className={styles.propertyName}>
+                    <IconClock className={styles.propertyIcon} />
+                    <p className="text-preset-6">Last edited</p>
+                  </div>
+                  <p className="text-preset-6">29 Oct 2024</p>
+                </div>
+                <hr />
+                <textarea
+                  value={note.content}
+                  className={`text-preset-5 text-color-neutral-800 ${styles.content}`}
+                  onChange={() => {}}
+                  aria-label="Content"
+                />
+              </section>
+              <footer className={styles.footer}>
+                <NoteFooter />
+              </footer>
+            </div>
+          </div>
+          <aside className={styles.sidebar}>
+            <NoteSiderbar />
+          </aside>
+        </div>
+      ) : (
+        <div className={styles.alternative}>
+          <p className="text-preset-5 text-color-neutral-800">Loading...</p>
+        </div>
+      )}
+    </>
   );
 }
