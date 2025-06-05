2a355cb3598f368a88691b4f13f5b7f7b7d35257
Christian <krisztian.radvanyi@gmail.com>
2025-06-05 12:00:37 +0200

    Refactor: replaced the SettingsPage component content to the actual settings page for a more next.js idiomatic implementation.

--- app/dashboard/settings/layout.js
+++ app/dashboard/settings/layout.js
@@ -1,14 +1,7 @@
-import SettingsPage from "@/app/_components/settings-page/settings-page";
-
 export const metadata = {
   title: "Settings",
 };
 
 export default function SettingsLayout({ children }) {
-  return (
-    <>
-      <SettingsPage />
-      {children}
-    </>
-  );
+  return children;
 }
--- app/dashboard/settings/page.js
+++ app/dashboard/settings/page.js
@@ -1,3 +1,24 @@
-export default function Settings() {
-  return null;
+"use client";
+
+import styles from "./page.module.css";
+import AllSettings from "@/app/_components/all-settings/all-settings";
+import { useAppState, NOTE, SETTINGS } from "@/app/_lib/app/use-app-state";
+
+export default function SettingsPage() {
+  const { page } = useAppState();
+
+  return (
+    <div className={styles.page}>
+      <aside
+        className={`${styles.allNotes} ${
+          page === SETTINGS ? styles.active : ""
+        }`}
+      >
+        <AllSettings />
+      </aside>
+      <article
+        className={`${styles.note} ${page === NOTE ? styles.active : ""}`}
+      ></article>
+    </div>
+  );
 }
