54eef92826387d0b2e7ed01d99718aea95935814
Christian <krisztian.radvanyi@gmail.com>
2025-07-18 06:28:31 +0200

    Test: Add UI unit tests for the "Archive one of my notes" story

--- app/_components/notes-page/notes-page.test.js
+++ app/_components/notes-page/notes-page.test.js
@@ -721,8 +721,8 @@ describe("NotesPage - Browse my archived notes", () => {
   });
 });
 
-describe.only("NotesPage - Archive one of my notes", () => {
-  it.only("does not show the note in the list after archivation - portable", async () => {
+describe("NotesPage - Archive one of my notes", () => {
+  it("does not show the note in the list after archivation - portable", async () => {
     /*
     GIVEN I opened the page of a specific note
     WHEN I click on the archive button
@@ -753,25 +753,42 @@ describe.only("NotesPage - Archive one of my notes", () => {
     await waitFor(() => {
       const allNotes = screen.getByTestId("All Notes");
       expect(within(allNotes).getByText("First Note")).toBeInTheDocument();
+      expect(
+        within(allNotes).queryByText("Second Note")
+      ).not.toBeInTheDocument();
     });
   });
 
-  it("shows the note in the archived list after archivation", () => {
-    /*
-    GIVEN I opened the page of a specific note
-    WHEN I click on the archive button
-    THEN I can see the note in the list of my archived notes
-    */
-    // Written by me later
-  });
-
-  it("shows toast message on successful archivation", () => {
+  it("shows toast message on successful archivation", async () => {
     /*
     GIVEN I opened the page of a specific note
     WHEN I click on the archive button
     THEN the note is stored in the database as archived note
     AND I can see a successfully archived toast message
     */
-    // Written by me later
+
+    // Arrange
+    readAllNotesAction.mockResolvedValue(notes);
+    readNoteAction.mockResolvedValue(notes[1]);
+    useAppState.mockReturnValue({ page: NOTE, noteId: "2" });
+    render(
+      <NotesPageWrapper>
+        <NotesPage />
+      </NotesPageWrapper>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByLabelText("Archive Note")).toBeInTheDocument();
+    });
+
+    const archiveButton = screen.getByLabelText("Archive Note");
+
+    // Act
+    await userEvent.click(archiveButton);
+
+    // Assert
+    await waitFor(() => {
+      expect(screen.queryByText("Note archived.")).not.toBeInTheDocument();
+    });
   });
 });
