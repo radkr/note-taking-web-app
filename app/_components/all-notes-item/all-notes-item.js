5ee05f810da9f37a1a0bf10bb6854caccb43125c
Christian <krisztian.radvanyi@gmail.com>
2025-07-27 18:49:22 +0200

    Browse my notes with a specific search term: - Open my note from the search results

--- app/_components/all-notes-item/all-notes-item.js
+++ app/_components/all-notes-item/all-notes-item.js
@@ -2,18 +2,41 @@ import Link from "next/link";
 
 import styles from "./all-notes-item.module.css";
 import { formatDate } from "@/app/_lib/utils";
+import {
+  ACTIVE,
+  ARCHIVED,
+  SEARCH,
+  useAppState,
+} from "@/app/_lib/app/use-app-state";
 
 export default function AllNotesItem({ note, id }) {
+  const { subPage, term } = useAppState();
   const isSelected = note._id == id;
 
   const formattedDate = note?.updatedAt
     ? formatDate(note.updatedAt)
     : "unknown";
 
+  let hrefSubPage;
+
+  switch (subPage) {
+    case ACTIVE:
+      hrefSubPage = "";
+      break;
+    case ARCHIVED:
+      hrefSubPage = "archived/";
+      break;
+    case SEARCH:
+      hrefSubPage = "search/";
+      break;
+    default:
+      hrefSubPage = "";
+  }
+
   return (
     <li className={`${styles.note} ${isSelected ? styles.selected : ""}`}>
       <Link
-        href={`/notes/${note.isArchived ? "archived/" : ""}${note._id}`}
+        href={`/notes/${hrefSubPage}${note._id}${term ? `?term=${term}` : ""}`}
         className={styles.noteCard}
       >
         <h2 className="text-preset-3 text-color-neutral-950">
--- app/_components/all-notes-item/all-notes-item.test.js
+++ app/_components/all-notes-item/all-notes-item.test.js
@@ -4,6 +4,27 @@ import AllNotesItem from "./all-notes-item";
 
 const date = new Date("2024-06-01T12:00:00.000Z");
 
+// Mock the useAppState hook
+jest.mock("@/app/_lib/app/use-app-state", () => {
+  const originalModule = jest.requireActual("@/app/_lib/app/use-app-state");
+  return {
+    __esModule: true,
+    ...originalModule,
+    useAppState: jest.fn(() => ({
+      page: originalModule.NOTES,
+    })),
+  };
+});
+
+import {
+  useAppState,
+  NOTES,
+  NOTE,
+  ACTIVE,
+  ARCHIVED,
+  SEARCH,
+} from "@/app/_lib/app/use-app-state";
+
 describe("AllNotesItem - Browse all my notes", () => {
   it("shows if the notes are untitled", () => {
     /*
@@ -13,6 +34,12 @@ describe("AllNotesItem - Browse all my notes", () => {
     WHEN I browse the list of my notes
     THEN I can read "Untitled note" in the place of the title on the notes without one
     */
+
+    useAppState.mockReturnValue({
+      page: NOTES,
+      subPage: ACTIVE,
+    });
+
     const note = { _id: "1", updatedAt: date };
     render(<AllNotesItem note={note} />);
     expect(screen.getByText("Untitled Note")).toBeInTheDocument();
@@ -26,6 +53,12 @@ describe("AllNotesItem - Browse all my notes", () => {
     WHEN I browse the list of my notes
     THEN I can see the title of each note that has one
     */
+
+    useAppState.mockReturnValue({
+      page: NOTES,
+      subPage: ACTIVE,
+    });
+
     const note = {
       _id: "2",
       title: "Titled Note",
@@ -43,6 +76,11 @@ describe("AllNotesItem - Browse all my notes", () => {
     THEN I can see when were the notes last edited
     */
 
+    useAppState.mockReturnValue({
+      page: NOTES,
+      subPage: ACTIVE,
+    });
+
     const note = {
       _id: "3",
       title: "Another Note",
@@ -59,6 +97,12 @@ describe("AllNotesItem - Browse all my notes", () => {
       WHEN I click on one of my note in the list
       THEN I get to the page of that specific note
       */
+
+    useAppState.mockReturnValue({
+      page: NOTES,
+      subPage: ACTIVE,
+    });
+
     const note = {
       _id: "3",
       title: "Another Note",
@@ -69,6 +113,35 @@ describe("AllNotesItem - Browse all my notes", () => {
     expect(screen.getByText(note.title).closest("a")).toHaveAttribute(
       "href",
       "/notes/3"
+    );
+  });
+});
+
+describe("AllNotesItem - Browse my notes with a specific search term", () => {
+  it("opens my note", async () => {
+    /*
+      GIVEN I have created some notes already
+      AND the list of my notes is available on the client
+      WHEN I click on one of my note in the list
+      THEN I get to the page of that specific note
+      */
+
+    useAppState.mockReturnValue({
+      page: NOTES,
+      subPage: SEARCH,
+      term: "myTerm",
+    });
+
+    const note = {
+      _id: "3",
+      title: "Another Note",
+      updatedAt: date,
+    };
+    render(<AllNotesItem note={note} />);
+    // Should navigate to the note's page
+    expect(screen.getByText(note.title).closest("a")).toHaveAttribute(
+      "href",
+      "/notes/search/3?term=myTerm"
     );
   });
 });
